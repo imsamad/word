c77ac1815b552fc134abbfa0ce1613603580a459
AbdusSamad <imsamad00@gmail.com>
2022-09-13 17:56:34 +0530

    proper format

--- demo.js
+++ demo.js
@@ -14,13 +14,13 @@
 
   function displayResult(result) {
     const outputBox = document.getElementById("output");
-
+    console.log(`):- DOC `, result.value);
     const questionDocument = new DOMParser().parseFromString(
       result.value,
       "text/html"
     );
 
-    console.log(`questionDocument `, questionDocument.body);
+    console.log(`):- questionDocument `, questionDocument.body);
 
     let questionSets = questionDocument
       .querySelector("body")
@@ -38,25 +38,67 @@
       let allQuestions = questionSet.querySelectorAll(":scope > li");
 
       allQuestions.forEach((q) => {
-        const question = q.childNodes[0].textContent;
+        let qu = "";
+        q.childNodes.forEach((child) => {
+          if (["ol", "ul"].includes(child.nodeName.toLowerCase())) return;
+          // console.log("child ", child);
+          // console.log("nodeValue ", child.nodeValue);
+          // console.log(`innerHTML `, child.innerHTML);
+          // console.log(`textContent `, child.textContent);
+          // // console.log("getTag ", getTag(child));
+          const isHTMLtag = child.tagName?.toLowerCase();
+          // console.log(`isHTMLtag `, isHTMLtag);
+          if (isHTMLtag == "img") {
+            let img = "<img";
+            for (var i = 0; i < child?.attributes?.length; i++) {
+              var attrib = child.attributes[i];
+              img += ` ${attrib.name}="${attrib.value}" `;
+              // img += " " + attrib.name + "=" + attrib.value;
+            }
+            // child.attributes.forEach((attrib) => {
+            //   img += ` ${attrib.name}=${attrib.value}`;
+            // });
+            // img+=child.attributes
+            // console.log("child img", child.attributes);
+            img += "/>";
+            qu += img;
+            return;
+          }
+          if (isHTMLtag) {
+            let ch = `<${isHTMLtag}>${child.textContent}</${isHTMLtag}>`;
+            qu += ch;
+            return;
+          }
+
+          qu += child.textContent;
+        });
+        console.log(`question `, qu);
+        // const question = q.childNodes[0].textContent;
+        const question = qu;
 
         let optionsULorOL = q.querySelector(":scope > ol");
 
         if (!optionsULorOL) optionsULorOL = q.querySelector(":scope > ul");
         // No options provided
-        if (!optionsULorOL) return;
+        if (!optionsULorOL) {
+          // return;
+        }
 
-        let optionsDOM = optionsULorOL.querySelectorAll(":scope > li");
-        if (!optionsDOM.length) return;
+        let optionsDOM = optionsULorOL?.querySelectorAll?.(":scope > li");
+        if (!optionsDOM?.length) {
+          // return;
+        }
         let options = [];
-        optionsDOM.forEach((op) => {
+        optionsDOM?.forEach((op) => {
           options.push(op.textContent);
         });
 
         questions.push({ question, options });
       });
     });
+
     console.log("questions ", questions);
+
     const quest = questions
       .map((q) => {
         const options = q.options
